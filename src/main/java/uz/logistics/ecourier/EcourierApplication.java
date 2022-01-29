da6317e9dc7ab0e796ab9aaae28ee4bc6d707df7
Shohjahon <r.shohjahon95@gmail.com>
2022-01-29 17:28:16 +0500

    initial commit

---
+++ src/main/java/uz/logistics/ecourier/EcourierApplication.java
@@ -0,0 +1,13 @@
+package uz.logistics.ecourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcourierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcourierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/logistics/ecourier/EcourierApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.logistics.ecourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcourierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
